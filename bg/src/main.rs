bb2b818dbe49cfe959332f1bc34e173f676c3131
Spyder337 <owsley.wood@gmail.com>
2024-03-14 16:44:41 -0700

    Basic CLI interface for the library.
    Enables testing directory outputs.

--- bg/src/main.rs
+++ bg/src/main.rs
@@ -1,3 +1,34 @@
+use std::error::Error;
+
+use bg_lib::{generators::RustGenerator, Generator, ProjectBuilder, ProjectType};
+use clap::Parser;
 
 
-fn main() {}
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short = 'b', value_enum, default_value="rust")]
+    build_system: ProjectBuilder,
+    #[arg(short = 't', value_enum, default_value="bin")]
+    build_type: ProjectType,
+    #[arg(required=true)]
+    p_name: String,
+    #[arg(required=false)]
+    libs: Vec<String>
+}
+
+fn main() -> Result<(), Box<dyn Error>>{
+    //  Note --help returns after this function.
+    let cli = Cli::parse();
+
+    let gen: Box<dyn Generator>;
+    match cli.build_system {
+        ProjectBuilder::Build2 => todo!(),
+        ProjectBuilder::Python => todo!(),
+        ProjectBuilder::Rust => gen = Box::new(RustGenerator),
+    }
+    
+    gen.create(cli.build_type, &cli.p_name, &cli.libs)?;
+
+    return Ok(());
+}
